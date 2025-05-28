dc1dfbe040dee2f6f25aebd2bf57f93e37d5605e
Kage-lab <kageyama@caliper.co.jp>
2025-05-28 21:17:19 +0900

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+
+import dynamic from 'next/dynamic';
+const GrowthAssistantApp = dynamic(() => import('../components/GrowthAssistantApp'), { ssr: false });
+
+export default function Home() {
+  return <GrowthAssistantApp />;
+}
