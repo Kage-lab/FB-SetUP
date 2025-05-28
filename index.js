04eb7a2ef6835c7c722484593b6ac28f431d6802
Kage-lab <kageyama@caliper.co.jp>
2025-05-28 21:16:35 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-import dynamic from 'next/dynamic';
-const GrowthAssistantApp = dynamic(() => import('../components/GrowthAssistantApp'), { ssr: false });
-
-export default function Home() {
-  return <GrowthAssistantApp />;
-}
